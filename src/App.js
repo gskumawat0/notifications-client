88279a97bb3244dea287663f428c0d045692e521
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2019-08-11 14:25:42 +0530

    add socket.io to listen for notifications

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import './App.css';
+import socketIOClient from "socket.io-client";
 
 
 class App extends Component {
 	constructor() {
 		super();
-		
+		this.state = {
+			notifications: [],
+			unReadNotifications: 0,
+			drawerOpen: false
+		};
+	}
+
+	componentDidMount = () => {
+		const socket = socketIOClient(process.env.REACT_APP_BACKEND_API_URL);
+		socket.on('connection', ()=>{
+			
+		})
+		socket.on('add_notification', (notification)=>{
+			let {notifications, unReadNotifications} = this.state;
+			notification.unRead = true;
+			this.setState({
+				notifications: [notification, ...notifications],
+				unReadNotifications: unReadNotifications + 1
+			})
+		})
 	}
 
 	render() {
